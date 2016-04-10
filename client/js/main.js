ce0e74cca0f745579c27cf51d12c3a82ac62aeb5
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-10 12:32:00 +0900

    client 書く方法を、lineTo＆moveToに変更

--- client/js/main.js
+++ client/js/main.js
@@ -3,21 +3,34 @@
     isMouseDown: false,
     element: null,
     context: null,
+    beforeMousePosX: 0,
+    beforeMousePosY: 0,
     setupCanvas: function() {
       canvas.element = document.getElementById("canvas");
       canvas.context = canvas.element.getContext("2d");
       canvas.context.fillStyle = "red";
-      canvas.element.addEventListener("mousedown", function() {
+      canvas.element.addEventListener("mousedown", function(event) {
         canvas.isMouseDown = true;
+        beforeMousePosX = event.clientX;
+        beforeMousePosY = event.clientY;
       });
       canvas.element.addEventListener("mouseup", function() {
         canvas.isMouseDown = false;
       });
+      canvas.element.addEventListener("mousemove", function(event) {
+        if (canvas.isMouseDown) {
+          canvas.context.lineWidth = 10;
+          canvas.context.beginPath();
+          canvas.context.moveTo(beforeMousePosX, beforeMousePosY);
+          canvas.context.lineTo(event.clientX, event.clientY);
+          canvas.context.lineCap = "round";
+          canvas.context.stroke();
+          beforeMousePosX = event.clientX;
+          beforeMousePosY = event.clientY;
+        }
+      });
       canvas.resizeCanvas();
       window.addEventListener("resize", canvas.resizeCanvas);
-    },
-    attachMouseMoveListener: function(listener) {
-      canvas.element.addEventListener("mousemove", listener);
     },
     resizeCanvas: function() {
       canvas.element.width = window.innerWidth;
@@ -26,7 +39,6 @@
   };
   document.addEventListener("DOMContentLoaded", function() {
     canvas.setupCanvas();
-    canvas.attachMouseMoveListener(mouseMoveCanvas);
     document.getElementById("btn-open-inspector").addEventListener("click", function() {
       document.getElementById("menu").classList.toggle("menu-open");
     });
@@ -34,13 +46,8 @@
       nodes.addEventListener("click", clickColorItem);
     });
   });
-  function mouseMoveCanvas(event) {
-    if (event.buttons === 1) {
-      canvas.context.beginPath();
-      canvas.context.arc(event.clientX, event.clientY, 10, Math.PI * 2, false);
-      canvas.context.fill();
-    }
-  }
+  var beforeMousePosX = -1;
+  var beforeMousePosY = -1;
   function clickColorItem() {
     if (this.style.backgroundColor === "white") {
       canvas.context.globalCompositeOperation = 'destination-out';
@@ -48,6 +55,6 @@
       canvas.context.globalCompositeOperation = 'source-over';
     }
     console.log(canvas.context.globalCompositeOperation);
-    canvas.context.fillStyle = this.style.backgroundColor;
+    canvas.context.strokeStyle = this.style.backgroundColor;
   }
 }();
