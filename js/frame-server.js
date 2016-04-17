045ef9d39c47f68be3df659b45f4f4faf0d923f0
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-17 11:30:18 +0900

    frameServerにコメントを追加、main.jsの書き方を修正

--- js/frame-server.js
+++ js/frame-server.js
@@ -1,3 +1,5 @@
+// frame の追加・削除と、DOM上でのCanvasの追加・削除を連動させる
+
 function FrameServer(elem) {
   
 }
@@ -9,3 +11,5 @@ FrameServer.prototype.append = function() {
 FrameServer.prototype.remove = function() {
   
 };
+
+module.exports = FrameServer;
--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var ParaparaCanvas = require("./parapara-canvas");
 var Frame = require("./frame");
 var FrameServer = require("./frame-server");
 
-var frameServer = new FrameServer();
+var frameServer;
 document.addEventListener("DOMContentLoaded", function() {
-  
+  // document.body を引数にするのは超絶良くないが、
+  // html を変える必要が有るため、後で直さなくてはならない
+  frameServer = new FrameServer(document.body);
 });
