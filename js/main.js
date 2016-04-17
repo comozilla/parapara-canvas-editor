eef8209fc1702bc9793ade07f7113a27a6daf400
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-17 11:45:56 +0900

    ui から色や太さを変えられるよう、draw-tools.js を追加

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var ParaparaCanvas = require("./parapara-canvas");
 var Frame = require("./frame");
 var FrameServer = require("./frame-server");
+var drawTools = require("./draw-tools");
 
 var frameServer;
+var colorPicker, lineWidthPicker;
 document.addEventListener("DOMContentLoaded", function() {
   // document.body を引数にするのは超絶良くないが、
   // html を変える必要が有るため、後で直さなくてはならない
   frameServer = new FrameServer(document.body);
+  colorPicker = new drawTools.ColorPicker(document.getElementById("menu-colors"));
+  lineWidthPicker = new drawTools.LineWidthPicker(document.getElementById("menu-line-width"));
 });
