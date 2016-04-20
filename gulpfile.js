57811b2d43340b1bea87d714669c8bf2c847f7de
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-20 16:50:06 +0900

    UglifyJsPlugin時にエラーが出ないよう、babel-loaderを導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
 var gulp = require("gulp");
-var webpack = require("gulp-webpack");
-var rename = require("gulp-rename")
+var gutil = require("gulp-util");
+var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
 var minimist = require("minimist");
 
 var config = require("./webpack.config.js");
 
 gulp.task("webpack", function(callback) {
-  return gulp.src("js/main.js").pipe(webpack()).pipe(rename({ basename: "bundle" })).pipe(gulp.dest("js/"));
+  var env = minimist(process.argv.slice(2));
+  var options = Object.create(config);
+  if (env["min"]) {
+    options.output.filename = "./js/bundle.min.js";
+    options.plugins = [new webpack.optimize.UglifyJsPlugin()];
+  }
+  var compiler = webpack(options, function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString());
+    callback();
+  });
+  if (env["dev-server"]) {
+    // webpack-dev-serverで監視する
+    new WebpackDevServer(compiler, { }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+  }
 });
