da326931026f9cf5be324ee1a0f8db4843aae4d5
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-20 20:31:47 +0900

    テストケースをこのHTMLに合わせて変更

--- test/draw-tools.test.js
+++ test/draw-tools.test.js
@@ -1,21 +1,32 @@
 var assert = require("power-assert");
 var jsdomify = require("jsdomify").default;
+var drawTools = require("./../js/draw-tools.js");
 
 describe("draw-tools.js", function() {
-  var target;
+  var menuColorsElem;
   before(function() {
     // テストの開始時にDOMを作る
-    jsdomify.create('<!doctype html><html><body><div id="content"></div></body></html>');
-    target = document.querySelector('#content');
+    jsdomify.create('<!doctype html><html lang="ja"><head></head><body><ul id="menu-colors"><li style="background-color: black;"></li></ul></body></html>');
+    menuColorsElem = document.getElementById("menu-colors");
   });
   beforeEach(function() {
-    target.innerHTML = 'hoge';
+    menuColorsElem.innerHTML = "";
   });
   after(function() {
     // テストが終わったら削除する
     jsdomify.destroy();
   });
-  it('Can render html', function() {
-    assert(target.innerHTML === "hoge");
+  it("ColorPicker addColorUI", function() {
+    var colorPicker = new drawTools.ColorPicker(menuColorsElem, "white");
+    colorPicker.addColorUI("red");
+    assert(menuColorsElem.childNodes[0].style.backgroundColor === "red");
+  });
+  it("ColorPicker listenerCheck (static element)", function() {
+    // TODO: jsdomifyで、clickとかが取れるのか
+  });
+  it("ColorPicker listenerCheck (dynamic element)", function() {
+    var colorPicker = new drawTools.ColorPicker(menuColorsElem, "white");
+    colorPicker.addColorUI("blue");
+    
   });
 });
