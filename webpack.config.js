789df969a6bd3f5c6ae046e5b71186594c2385e9
hrl7 <hrlclb@gmail.com>
2016-04-21 23:09:13 +0900

    Fix query passed to babel-loader :  enable cacheDirectry

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
+          cacheDirectory: true,
           presets: ['es2015']
         }
       }
