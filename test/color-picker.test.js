c9359cb045a70688105610c79bee42aa4fb0802e
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-21 18:05:46 +0900

    color-picker.test.js テストケースをネストして追加

--- test/color-picker.test.js
+++ test/color-picker.test.js
@@ -2,4 +2,7 @@ var assert = require('power-assert');
 describe("ColorPicker", function () {
   it("has color attribute");
   it("has parent element");
+  describe("#addColorUI", function () {
+    it("fails when color is assigned not CSS3 style color");
+  });
 });
