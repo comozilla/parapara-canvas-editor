99b966fac6caf6dcefb8e2cfa320440c5ba79b9e
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-22 21:25:44 +0900

    いらなくなったwebpack-dev-serverを削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
 var minimist = require("minimist");
 var browserSync = require("browser-sync");
 
