009a93e7df4ad2f8c18757b9094e97f796718451
yusuke™ <yusuke20023@gmail.com>
2016-04-23 15:11:36 +0900

    fix #14

--- js/line-width-picker.js
+++ js/line-width-picker.js
@@ -2,8 +2,9 @@ function LineWidthPicker(elem, defaultLineWidth) {
   // このelem には、input[type="range"] 要素が入ってくる（はず）。
   this.element = elem;
   this.lineWidth = defaultLineWidth;
+  this.element.value = this.lineWidth //by yusuke endho
   this.element.addEventListener("change", event => {
-    this.lineWidth = event.target.value;
+  this.lineWidth = event.target.value;
   });
 }
 
