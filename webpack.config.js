fc02a57f5bca358bb23823187267a18420407001
shundroid <ra24483@qb3.so-net.ne.jp>
2016-04-23 18:26:54 +0900

    ダブルクォーテーションに修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@
 var webpack = require("webpack");
 module.exports = {
   cache: true,
-  entry: './js/main.js',
+  entry: "./js/main.js",
   output: {
     path: __dirname,
-    filename: './js/build/bundle.js'
+    filename: "./js/build/bundle.js"
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: "babel",
         query: {
           cacheDirectory: true,
-          presets: ['es2015']
+          presets: ["es2015"]
         }
       },
       { test: /\.css$/, loader: "style-loader!css-loader" },
