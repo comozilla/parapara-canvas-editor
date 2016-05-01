19288870ea95516ddde28325e54e3d06e8bb1c38
shundroid <ra24483@qb3.so-net.ne.jp>
2016-05-01 18:43:20 +0900

    publisherのバグを修正

--- js/publisher.js
+++ js/publisher.js
@@ -1,14 +1,20 @@
 // Publisherは、データを保存せず、外部へ変更を知らせる機能に絞る。
 function Publisher() {
-  this.observers = [];
+  this.observers = {};
 }
 
 Publisher.prototype.subscribe = function(type, observer) {
-  this.observers.push(observer);
+  if (typeof this.observers[type] === "undefined") {
+    this.observers[type] = [];
+  }
+  this.observers[type].push(observer);
 };
 
 Publisher.prototype.publish = function(type, nextData) {
-  this.observers.forEach(observer => {
+  if (typeof this.observers[type] === "undefined") {
+    this.observers[type] = [];
+  }
+  this.observers[type].forEach(observer => {
     observer(nextData);
   });
 };
