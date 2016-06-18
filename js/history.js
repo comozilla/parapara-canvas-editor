0b73f7ae07b5e53b471fde5dcb3cd70961088781
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-06-18 01:14:45 -0700

    importでframescontrollerを引数で呼ぶように。

--- js/history.js
+++ js/history.js
@@ -1,8 +1,23 @@
 import eventPublisher from "./publisher";
 
-function test(canvas){
+function test(framescontroller){
+    
+    
     eventPublisher.subscribe("drawState",function(data){
+        let frames=[];
+        //framesは各レイヤを入れる。
+        
+        
         console.log(data);
+        if(data=="drawing"){
+            
+            console.log("書いている。書き始めた")
+            
+        }else if(data=="idling"){
+            
+            console.log("書き終わった。frameを保存する。")
+        }
+        
     });
 }
 
